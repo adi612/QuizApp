fd67f28227df21e1fc21d6b68b52463ad3d898ba
adi612 <69754352+adi612@users.noreply.github.com>
2024-08-08 22:06:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 const questions=[
     {
-        question:"How black is Aditya?",
+        question:"What is Capital of India?",
         answers:[
-            {text:'Nigger',correct:false},
-            {text:'Light-Skin',correct:false},
-            {text:'kala',correct:true},
-            {text:'Dark-skin',correct:false},
+            {text:'UP',correct:false},
+            {text:'Maharastra',correct:false},
+            {text:'Delhi',correct:true},
+            {text:'Goa',correct:false},
         ]
     },
     {
-        question:"Who is the shortest?",
+        question:"Which is the largest coffee-producing state of India?",
         answers:[
-            {text:'Tanmay',correct:true},
-            {text:'Anmol',correct:false},
-            {text:'Sannit ki choti luli',correct:false},
-            {text:'Dada',correct:false},
+            {text:'Maharastra',correct:true},
+            {text:'Kerala',correct:false},
+            {text:'Karnataka',correct:false},
+            {text:'UP',correct:false},
         ]
     },
     {
-        question:"Whos the thickest?",
+        question:"Which state has the largest area?",
         answers:[
             {text:'Zack',correct:true},
             {text:'Dada',correct:false},
@@ -27,68 +27,15 @@ const questions=[
         ]
     },
     {
-        question:"Dada ka weight(Its accurate)?",
+        question:"Which is the national tree of India?",
         answers:[
-            {text:'72kg',correct:false},
-            {text:'65kg',correct:false},
-            {text:'80kg',correct:true},
-            {text:'90kg',correct:false},
+            {text:'Manggo',correct:false},
+            {text:'Apple',correct:false},
+            {text:'Banyan',correct:true},
+            {text:'Grapes',correct:false},
         ]
     },
-    {
-        question:"Zack's full name",
-        answers:[
-            {text:'Atharav dadaranla',correct:false},
-            {text:'Atharav darandale',correct:true},
-            {text:'Atharav darundale',correct:false},
-            {text:'Atharav darendra',correct:false},
-        ]
-    },
-    {
-        question:"When was group Created(original)",
-        answers:[
-            {text:'15 june 2021',correct:false},
-            {text:'26 dec',correct:false},
-            {text:'7 jan 2021',correct:false},
-            {text:'20 july 2021',correct:true},
-        ]
-    },
-    {
-        question:"Subse Cringe?",
-        answers:[
-            {text:'Zack',correct:false},
-            {text:'sannit',correct:true},
-            {text:'aditya',correct:false},
-            {text:'yog',correct:false},
-        ]
-    },
-    {
-        question:"Indra ka age?",
-        answers:[
-            {text:'legally can date 13 year old girl/boy',correct:false},
-            {text:'15',correct:false},
-            {text:'17',correct:true},
-            {text:'22',correct:false},
-        ]
-    },
-    {
-        question:"Who looks like asia/chinesh,japanesh,small eye",
-        answers:[
-            {text:'sannit areakar',correct:true},
-            {text:'sannit arekar',correct:true},
-            {text:'sannit chutikar',correct:true},
-            {text:'sannit areilkar',correct:true},
-        ]
-    },
-    {
-        question:"Who is the gayesh?",
-        answers:[
-            {text:'kelker',correct:false},
-            {text:'granditya gara(amin)',correct:true},
-            {text:'rajiya',correct:true},
-            {text:'om gule',correct:true},
-        ]
-    },
+   
     
 
 ];
